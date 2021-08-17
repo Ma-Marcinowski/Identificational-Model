dd939f24a1c9c690cdd69e90697aeee24c473df8
Ma. Marcinowski <39803228+Ma-Marcinowski@users.noreply.github.com>
2021-08-17 16:02:49 +0200

    Model Training and Model v3.0

--- Models/Model_Training_v3.0.py
+++ Models/Model_Training_v3.0.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 from tensorflow.keras.models import Model, load_model
-from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras.losses import categorical_crossentropy, binary_crossentropy
 from tensorflow.keras.layers import Input, Conv2D, Dense, MaxPooling2D
 from tensorflow.keras.layers import BatchNormalization, GaussianDropout
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
@@ -114,7 +114,9 @@ model = Model(inputs=[input], outputs=[features, output])
 
 Adam = tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, amsgrad=False)
 
-model.compile(optimizer=Adam, loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam, 
+              loss={'features': 'binary_crossentropy', 'output': 'categorical_crossentropy'}, 
+              metrics=['accuracy'])
 
 csv_logger = tf.keras.callbacks.CSVLogger('/path/IM_v3.0_Training_Log.csv', separator=',', append=True)
 
@@ -145,6 +147,6 @@ history = model.fit(x=TrainSeq,
                     verbose=1,
                     validation_freq=1,
                     initial_epoch=0,
-                    epochs=60)
+                    epochs=90)
 
 #model.save(filepath='/path/IM_v3.0.h5', overwrite=True, include_optimizer=True, save_format='h5')
